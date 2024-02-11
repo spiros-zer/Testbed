df54a13e2da6f4479f99e9a8df41dc654f478ab3
CORP\s_zervos <s_zervos@wargaming.net>
2024-02-11 12:35:24 +0200

    Update of Cartesian Coordinates submodule

--- App/Source/Testbed.cpp
+++ App/Source/Testbed.cpp
@@ -3,9 +3,24 @@
 
 #include<iostream>
 
-#include "Core\CartesianCoordinates2D.h"
+#include "CartesianCoordinates/CartesianCoordinates2D.h"
+#include "PolarCoordinates/PolarCoordinates.h"
+
+void CheckCartesian();
+void CheckPolar();
 
 int main(int argc, char* argv[])
+{
+    std::cout << "/**** CHECKING CARTESIAN COORDINATES LIB" << '\n';
+    CheckCartesian();
+
+    std::cout << "/**** CHECKING POLAR COORDINATES LIB" << '\n';
+    CheckPolar();
+    
+    return 0;
+}
+
+void CheckCartesian()
 {
     /** INITIALIZATION TESTING */
     std::cout << "/** INITIALIZATION TESTING */" << '\n';
@@ -37,6 +52,38 @@ int main(int argc, char* argv[])
     std::cout << "PointD = PointA + PointC =";
     PointD.Print();
     std::cout << '\n';
+}
+
+void CheckPolar()
+{
+    /**** INITIALIZATION TESTTING */
+    std::cout << "/**** INITIALIZATION TESTING */" << '\n';
     
-    return 0;
+    const PolarCoordinates X(1, 90);
+    std::cout << "X ";
+    X.Print();
+    std::cout << '\n';
+
+    const PolarCoordinates Y(4, 180);
+    std::cout << "Y ";
+    Y.Print();
+    std::cout << '\n';
+
+    PolarCoordinates Z(1, 0);
+    std::cout<< "Z ";
+    Z.Print();
+    std::cout << '\n';
+
+    /**** OPERATOR TESTING */
+    std::cout << "/**** OPERATOR TESTING */" << '\n';
+
+    Z = X * Y;
+    std::cout << "Z = Χ * Υ = ";
+    Z.Print();
+    std::cout << '\n';
+
+    const PolarCoordinates K = Z / Z;
+    std::cout << "K = Z / Z = ";
+    K.Print();
+    std::cout << "\n";
 }
